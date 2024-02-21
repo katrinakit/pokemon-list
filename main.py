5389098674b8de1631975759f967616e5358a074
katrina laura terentjeva <you@example.com>
2024-02-21 10:20:15 +0200

    izmaiņas

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ with open('pokemon.csv', newline='') as csv_file:
 # print(pokemons)
 
 print("Pokemon list command:")
-
+terminator=pokemons.copy()
 while True:
     print("1. Get pokemon by sequence number")
     print("2. Sort by A-Z")
@@ -26,19 +26,27 @@ while True:
     choice = input("Enter your choice (1-6): ")
 
     if choice == '1':
+        a=int(input("please put the pokemons number in:"))
+        print(pokemons[a-1])
         # https://www.w3schools.com/python/python_lists_access.asp
         pass
     elif choice == '2':
+        terminator.sort()
+        print(terminator)
         # https://www.w3schools.com/python/python_lists_sort.asp
         pass
     elif choice == '3':
+        terminator.sort(reverse = True)
+        print(terminator)
         # https://www.w3schools.com/python/python_lists_sort.asp
         pass
     elif choice == '4':
+        print("please put the pokemons name/pokemons letter here:")
         # https://www.w3schools.com/python/python_lists_comprehension.asp
         # https://www.w3schools.com/python/ref_string_startswith.asp
         pass
     elif choice == '5':
+        print("please put the pokemons names length here:")
         # https://www.w3schools.com/python/python_lists_comprehension.asp
         pass
     elif choice == '6':
